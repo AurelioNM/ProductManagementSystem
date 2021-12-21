d43a0f295a6c40c63725e6c0e058794bdb178a4e
AurelioNM <aureliomororo300@gmail.com>
2021-12-21 17:54:21 -0300

    Starting to build the contact with public API

---
+++ src/main/java/br/com/Impact/ProductManagementSystem/model/Currency.java
@@ -0,0 +1,20 @@
+package br.com.Impact.ProductManagementSystem.model;
+
+public enum Currency {
+    BRL,
+    USD,
+    CAD,
+    GBP,
+    ARS,
+    BTC,
+    LTC,
+    EUR,
+    JPY,
+    CHF,
+    AUD,
+    CNY,
+    ILS,
+    ETH,
+    XRP,
+    DOGE
+}
